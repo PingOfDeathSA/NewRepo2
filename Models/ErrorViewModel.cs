81c4cc361170b8ed5d4018165345affb745ca17f
PingOfDeathSA <ronaldnt8@gmail.com>
2023-03-05 11:33:38 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_com.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
